35f3897fb079214390660eabe9743cb45edb5521
woldin9711 <woldin9711@gmail.com>
2022-06-05 16:09:05 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require("browser-sync");
+const gulpStylelint = require("gulp-stylelint");
+
+function style () {
+    return gulp.src("./css/**/*.scss")
+            .pipe((sass()))
+            .pipe(gulp.dest("./css"))
+            .pipe(browserSync.stream())
+}
+
+function watch () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+    gulp.watch("./css/**/*.scss", style);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
